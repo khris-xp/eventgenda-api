5affe729374c0ab17403215b1d27d5af523aac41
Tanathip Pona <nm3207petchza@gmail.com>
2024-09-05 11:13:16 +0700

    add middleware for location router

--- routes/location.route.ts
+++ routes/location.route.ts
@@ -7,11 +7,8 @@ const locationRouter: Router = express.Router();
 locationRouter.get('/', locationController.getLocations);
 locationRouter.get('/:id', locationController.getLocation);
 locationRouter.get('/name/:name', locationController.getLocationByName);
-// locationRouter.post('/', authAdmin, locationController.createLocation);
-// locationRouter.put('/:id', authAdmin, locationController.updateLocation);
-// locationRouter.delete('/:id', authAdmin, locationController.deleteLocation);
-locationRouter.post('/', locationController.createLocation);
-locationRouter.put('/:id', locationController.updateLocation);
-locationRouter.delete('/:id', locationController.deleteLocation);
+locationRouter.post('/', authAdmin, locationController.createLocation);
+locationRouter.put('/:id', authAdmin, locationController.updateLocation);
+locationRouter.delete('/:id', authAdmin, locationController.deleteLocation);
 
 export default locationRouter;

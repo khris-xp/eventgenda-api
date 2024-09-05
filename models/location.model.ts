82bf3e41d062c23b38038950d7fb7750bffd9f1d
Tanathip Pona <nm3207petchza@gmail.com>
2024-09-05 11:14:18 +0700

    fix default thumbnail in location model

--- models/location.model.ts
+++ models/location.model.ts
@@ -6,7 +6,7 @@ const locationSchema = new Schema<LocationType>({
   name: { type: String, required: true },
   location: { type: String, required: true },
   prices: { type: Number, default: 0 },
-  thumbnail: { type: String, default: '' },
+  thumbnail: { type: String, default: 'https://www.contentviewspro.com/wp-content/uploads/2017/07/default_image.png' },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });

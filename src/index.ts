365fd2530fad0abe6584f0cca741c65bef760c36
Taarrz <65010395@kmitl.ac.th>
2024-09-06 23:43:44 +0700

    runtest

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,15 @@
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
 import dotenv from 'dotenv';
 import express, { Express, Request, Response } from 'express';
-import fileUpload from 'express-fileupload';
-import mongoose from 'mongoose';
-import blogRouter from '../routes/blog.route';
-import userRouter from '../routes/user.route';
-import locationRouter from '../routes/location.route';
 
 dotenv.config();
 
 const app: Express = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(cors());
-app.use(
-  fileUpload({
-    useTempFiles: true,
-  })
-);
-
-const port = process.env.PORT || 8081;
-const URL = process.env.MONGODB_URI;
+const port = process.env.PORT ?? 8081;
 
 app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Hello World!' });
 });
 
-app.use('/api/auth', userRouter);
-app.use('/api/blogs', blogRouter);
-app.use('/api/location', locationRouter);
-
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
-
-mongoose
-  .connect(URL as string)
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.log(err));

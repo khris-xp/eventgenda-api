d2a7d093e4ee62d32b5390529a8c9c2f1e53d85c
Tanathip Pona <nm3207petchza@gmail.com>
2024-09-13 00:06:55 +0700

    feat: add findByOrg and updateRole function for use in Organization

--- repositories/user.repository.ts
+++ repositories/user.repository.ts
@@ -7,6 +7,26 @@ class UserRepository {
     if (!user) throw new Error('User does not exist.');
     return user;
   }
+
+  async findByOrg(orgId: string) {
+    if (!orgId) throw new Error('Organization does not exist.');
+    const user = await User.findOne({ organization: orgId }).select('-password');
+    if (!user) throw new Error('User does not exist.');
+    if (!user.role.includes("Organizer")) throw new Error('User is not an organizer.');
+    return user;
+  }
+
+  async updateRole(userId: string, role: string) {
+    const user = await this.findById(userId);
+    const validRoles = ['User', 'Organizer'];
+
+    if (role === 'Admin') throw new Error('Cannot update user to admin role.');
+    // role must be either 'User' or 'Organizer'
+    if (!validRoles.includes(role)) throw new Error('Invalid role.');
+    
+    user.role[0] = role;
+    return await user.save();
+  }
 }
 
 export default new UserRepository();

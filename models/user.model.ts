f77616c7b12a23cd040582d82e9a22c7898df26c
Tanathip Pona <nm3207petchza@gmail.com>
2024-09-13 00:04:07 +0700

    chore: update user module and role enum to have organization

--- models/user.model.ts
+++ models/user.model.ts
@@ -14,9 +14,10 @@ const userSchema = new Schema<UserType>({
   role: {
     type: [String],
     default: ['User'],
-    enum: ['User', 'Admin'],
+    enum: ['User', 'Admin', 'Organizer']
   },
-  profileImage: { type: String, default: '' },
+  profileImage: { type: String, default: 'https://shorturl.at/CQtT2' },
+  organization: { type: Schema.Types.ObjectId, ref: 'Organization' },
   history: [{ type: Schema.Types.ObjectId, ref: 'History' }],
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },

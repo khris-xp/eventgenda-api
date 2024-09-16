d4176a6810dd9d4201a8b9b3200f22aa36ecae4d
Tanathip Pona <nm3207petchza@gmail.com>
2024-09-17 00:28:40 +0700

    chore: delete update role function

--- repositories/user.repository.ts
+++ repositories/user.repository.ts
@@ -12,20 +12,8 @@ class UserRepository {
     if (!orgId) throw new Error('Organization does not exist.');
     const user = await User.findOne({ organization: orgId }).select('-password');
     if (!user) throw new Error('User does not exist.');
-    if (!user.role.includes("Organizer")) throw new Error('User is not an organizer.');
+    if (user.role !== 'organizer') throw new Error('User is not an organizer.');
     return user;
-  }
-
-  async updateRole(userId: string, role: string) {
-    const user = await this.findById(userId);
-    const validRoles = ['User', 'Organizer'];
-
-    if (role === 'Admin') throw new Error('Cannot update user to admin role.');
-    // role must be either 'User' or 'Organizer'
-    if (!validRoles.includes(role)) throw new Error('Invalid role.');
-    
-    user.role[0] = role;
-    return await user.save();
   }
 }
 

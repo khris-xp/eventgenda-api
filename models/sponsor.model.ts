0609f3f2f6d4b4afc80d7a0d9946435c7be43172
Tanathip Pona <nm3207petchza@gmail.com>
2024-09-16 23:44:29 +0700

    chore: change funding to amount and add type to indicate funding or donate

--- models/sponsor.model.ts
+++ models/sponsor.model.ts
@@ -5,7 +5,8 @@ const { Schema } = mongoose;
 const sponsorSchema = new Schema<SponsorType>({
   user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   event: { type: Schema.Types.ObjectId, ref: 'Event', required: true },
-  funding: { type: Number, default: 0 },
+  amount: { type: Number, default: 0 },
+  type: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });

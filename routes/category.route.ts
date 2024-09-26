eb89e6afb5a9641a31acf2b88f52c0795757cc02
khris-xp <khrisbharmmano@gmail.com>
2024-09-26 16:59:05 +0700

    fix: path api response

--- routes/category.route.ts
+++ routes/category.route.ts
@@ -2,19 +2,18 @@ import express, { Router } from 'express';
 import CategoryController from '../controllers/category.controller';
 import authAdmin from '../middlewares/authAdmin.middleware';
 
-const categoryRouter : Router  = express.Router();
+const categoryRouter: Router = express.Router();
 
-categoryRouter.get('/categories', CategoryController.getCategories);
+categoryRouter.get('/', CategoryController.getCategories);
 
-categoryRouter.get('/categories/:id', CategoryController.getCategory);
+categoryRouter.get('/:id', CategoryController.getCategory);
 
-categoryRouter.get('/categories/name/:name', CategoryController.getCategoryByName);
+categoryRouter.get('/name/:name', CategoryController.getCategoryByName);
 
-categoryRouter.post('/categories', authAdmin ,CategoryController.createCategory);
+categoryRouter.post('/', authAdmin, CategoryController.createCategory);
 
-categoryRouter.put('/categories/:id', authAdmin, CategoryController.updateCategory);
+categoryRouter.put('/:id', authAdmin, CategoryController.updateCategory);
 
-categoryRouter.delete('/categories/:id',CategoryController.deleteCategory);
+categoryRouter.delete('/:id', CategoryController.deleteCategory);
 
 export default categoryRouter;
-

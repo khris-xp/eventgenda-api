71a8d547526746a15885c59e8505016f2c7b572e
Tanathip Pona <nm3207petchza@gmail.com>
2024-10-23 19:44:46 +0700

    refactor: change category type

--- types/event.d.ts
+++ types/event.d.ts
@@ -9,7 +9,7 @@ export type EventType = {
   title: string;
   description: string;
   limit: number;
-  category: CategoryType;
+  categories: CategoryType[];
   createdBy: UserType;
   eventStartDate: Date;
   eventEndDate: Date;

2c23649ffc5ca7f4819a23297a9b8c53788e2f4e
Tanathip Pona <nm3207petchza@gmail.com>
2024-10-29 01:51:10 +0700

    fix: fix update sponsor, validate event status and add credit to organization

--- common/dto/event.dto.ts
+++ common/dto/event.dto.ts
@@ -1,25 +1,26 @@
 export type CreateEventDto = {
-    title: string;
-    description: string;
-    limit: number;
-    categories: string[];
-    createdBy: string;
-    eventStartDate: Date;
-    eventEndDate: Date;
-    registrationStartDate: Date;
-    registrationEndDate: Date;
-    participants: string[];
-    sponsors: string[];
-    rules: string[];
-    projects: any;
-    prizes: number[];
-    thumbnail: string;
-    location: string;
-    amountRaised: number;
-    amountRequired: number;
-    status: string;
-  };
-  
-  export type fundingEventDto = {
-    amount: number;
-  };
+  title: string;
+  description: string;
+  limit: number;
+  categories: string[];
+  createdBy: string;
+  eventStartDate: Date;
+  eventEndDate: Date;
+  registrationStartDate: Date;
+  registrationEndDate: Date;
+  participants: string[];
+  sponsors: string[];
+  rules: string[];
+  projects: any;
+  prizes: number[];
+  thumbnail: string;
+  location: string;
+  amountRaised: number;
+  amountRequired: number;
+  status: string;
+};
+
+export type UpdateEventDto = CreateEventDto;
+export type FundingEventDto = { 
+  amount: number; 
+};
